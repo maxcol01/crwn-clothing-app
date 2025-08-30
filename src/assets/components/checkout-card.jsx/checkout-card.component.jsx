247775cdb3d4944a285c43ea1df4abe331c9d58a
Maxime Collet <maxcollet01@gmail.com>
2025-08-30 17:02:26 +0200

    added styel

--- src/assets/components/checkout-card.jsx/checkout-card.component.jsx
+++ src/assets/components/checkout-card.jsx/checkout-card.component.jsx
@@ -10,24 +10,18 @@ import {CartContext} from "../../../contexts/cart-icon.context.jsx";
 const CheckOutCard = ({info:{name, imageUrl, quantity, price, id}}) => {
     const {addItemToCart,removeItemToCart, deleteItemToCart} = useContext(CartContext);
     return (
-        <div className="selection-container">
-            <hr/>
-            <div className="selection-information">
-                <h1>{name}</h1>
+        <div className="checkout-item-container">
+            <div className="image-container">
                 <img src={imageUrl} alt={name}/>
-                <h2>{quantity}<i className="fa-solid fa-xmark"></i>${price} = ${quantity*price}</h2>
             </div>
-            <div className="selection-interaction">
-                <span>
-                    <button onClick={()=>removeItemToCart({id})}><i className="fa-solid fa-chevron-left"></i></button>
-                    {quantity}
-                    <button onClick={()=>addItemToCart({id})}><i className="fa-solid fa-chevron-right"></i></button>
-                </span>
-                <span>
-                    <button onClick={()=>deleteItemToCart({id})} ><i className="fa-solid fa-trash-can"></i></button>
-                </span>
-            </div>
-            <hr/>
+            <span className="name">{name}</span>
+            <span className="quantity">
+                <button onClick={() => removeItemToCart({id})}><i className="fa-solid fa-chevron-left"></i></button>
+                {quantity}
+                <button onClick={() => addItemToCart({id})}><i className="fa-solid fa-chevron-right"></i></button>
+            </span>
+            <span className="price">{price}</span>
+            <div className="remove-button"><button onClick={()=>deleteItemToCart({id})} ><i className="fa-solid fa-trash-can"></i></button></div>
         </div>
     )
 };
--- src/assets/routes/checkout/checkout.components.jsx
+++ src/assets/routes/checkout/checkout.components.jsx
@@ -2,13 +2,29 @@ import CheckOutCard from "../../components/checkout-card.jsx/checkout-card.compo
 import TotalCheckOut from "../../components/total-checkout/total-checkout.component.jsx";
 import {useContext} from "react";
 import {CartContext} from "../../../contexts/cart-icon.context.jsx";
-
+import "./checkout.styles.scss"
 
 const CheckOutRoute = () => {
 
     const {cartItems} = useContext(CartContext);
     return(
         <div>
+            <div className="checkout-container">
+                <div className="checkout-header">
+                    <div className="header-block">
+                        <span>Description</span>
+                    </div>
+                    <div className="header-block">
+                        <span>Quantity</span>
+                    </div>
+                    <div className="header-block">
+                        <span>Price</span>
+                    </div>
+                    <div className="header-block">
+                        <span>Remove</span>
+                    </div>
+                </div>
+            </div>
             {cartItems.map((cartItem) => (
                 <CheckOutCard key={cartItem.id} info={cartItem}/>
             ))}
