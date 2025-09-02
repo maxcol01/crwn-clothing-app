320db0c9116eec33b363f657c19b6311df241f7f
Maxime Collet <maxcollet01@gmail.com>
2025-09-02 20:53:58 +0200

    adding the sub routes for every categories

--- src/assets/routes/shop/shop.route.jsx
+++ src/assets/routes/shop/shop.route.jsx
@@ -1,23 +1,19 @@
-import { CategoriesContext} from "../../../contexts/categories.context.jsx";
-import { useContext, Fragment } from "react";
-//import ProductCard from "../../components/product-card/product-card.component";
-import CategoryPreview from "../../components/category-preview/category-preview.components.jsx";
+
 import "./shop.styles.scss";
 
+import CategoriesPreview from "../categories-preview/categories-preview.component.jsx";
+import {Routes, Route} from "react-router";
+import Category from "../category/category.component.jsx";
 
 const Shop = () => {
-  const {categoriesMap} = useContext(CategoriesContext);
 
   return(
-      <div className="shop-container">
-        {Object.keys(categoriesMap).map(title => {
-          const products = categoriesMap[title];
-          return <CategoryPreview key={title} title={title} products={products}/>
-        })}
+      <Routes>
+        <Route index element={<CategoriesPreview/>}/>
+        <Route path=":category" element={<Category/>}/>
+      </Routes>
 
-      </div>
-
-)
+  )
 }
 
 export default Shop;
