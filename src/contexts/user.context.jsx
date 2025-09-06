0f80ac44e90b6875d2b56eb2ed168791c9c530e8
Maxime Collet <maxcollet01@gmail.com>
2025-09-06 10:21:17 +0200

    change from useState hook to reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener } from "../assets/utils/firebase/firebase";
 import { createUserDocumentFromAuth } from "../assets/utils/firebase/firebase";
 // as the actual value we want to access = initial value (default not necessarly the first value)
@@ -9,10 +9,36 @@ export const UserContext = createContext({
     setCurrentUser: () => null
 });
 
-export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = {currentUser, setCurrentUser};
+export const USER_ACTION_TYPES = {
+    "SET_CURRENT_USER":"SET_CURRENT_USER"
+}
 
+const userReducer = (state, action) => {
+    const {type, payload} = action;
+    switch (type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type}`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+export const UserProvider = ({children}) => {
+    //const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+    const {currentUser} = state;
+    const setCurrentUser = (user) => {
+	    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER , payload: user})
+    }
+    const value = {currentUser, setCurrentUser};
     useEffect(()=>{
         const unsubscribe = onAuthStateChangedListener((user)=>{
             if (user){
@@ -27,7 +53,3 @@ export const UserProvider = ({children}) => {
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
 
-// example
-//<UserProvider>
-//    <app/>
-//</UserProvider>
