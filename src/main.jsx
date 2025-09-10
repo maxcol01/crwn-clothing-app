af8a482eb2d726741406523f5b1fa73a7f68bb2c
Maxime Collet <maxcollet01@gmail.com>
2025-09-10 17:31:32 +0200

    start building the store

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter} from "react-router"; {/* This is from the documention */}
+import {Provider} from "react-redux";
+
+import {store} from "./store/store.js";
 
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from './contexts/categories.context.jsx';
@@ -13,14 +16,16 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartContextProvider>
-            <App />
-          </CartContextProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <UserProvider>
+            <CategoriesProvider>
+              <CartContextProvider>
+                <App />
+              </CartContextProvider>
+            </CategoriesProvider>
+          </UserProvider>
+        </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,13 @@
-import {compose, createStore, applyMiddleware} from "redux";
+import {compose, legacy_createStore as createStore, applyMiddleware} from "redux";
 import {logger} from "redux-logger/src";
+import {rootReducer} from "./root-reducer.js";
+
+// create middleware
+
+const middlewares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middlewares))
 
 // we need a root-reducer (combination of all of our reducers)
 
+export const store = createStore(rootReducer, undefined, composedEnhancers);
